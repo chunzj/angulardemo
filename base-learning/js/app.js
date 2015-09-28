df9a29e4978677a8f0650260fbfb105518315f69
Chun Zujun <chunzj@huoli.com>
2015-09-28 18:28:19 +0800

    add directive learning demo

--- base-learning/js/app.js
+++ base-learning/js/app.js
@@ -2,7 +2,11 @@
  * Created by ChunZuJun on 2015/9/25.
  *
  */
-var myApp = angular.module('MyApp', ['dataPkg']);
+var myApp = angular.module('MyApp', ['dataPkg'], function () {
+  return {
+    debugInfoEnabled: true
+  };
+});
 myApp.constant('sex', ['男','女']);
 
 myApp.run(function ($rootScope) {
@@ -24,10 +28,39 @@ myApp.controller('logCtrl', function($scope, $log) {
 
 myApp.controller('invokeCtrl', function ($scope, data) {
   $scope.name = data.getName();
+  $scope.executeFn = function (fn, args) {
+    angular.identity(args);
+  }
+
+  $scope.age = $scope.executeFn(function (age) {
+    return age;
+  }, 27);
+
+  console.log(angular.identity($scope.executeFn));
+
 });
 
 myApp.controller('ngBindTemplateCtrl', function ($scope) {
   $scope.name = 'chunzj';
   $scope.salutation = '123';
+});
 
+myApp.controller('ngModelOptionsCtrl', function ($scope) {
+  var _name = 'chunzj';
+  $scope.name = function (name) {
+    if (name) {
+      console.log('set name');
+      _name = name;
+    } else {
+      console.log('get name');
+    }
+    return _name;
+  }
+});
+
+myApp.controller('pluralizeCtrl', function ($scope) {
+  $scope.person1 = 'Igor';
+  $scope.person2 = 'Misko';
+  $scope.person3 = 'Rick';
+  $scope.personCount = 1;
 });
